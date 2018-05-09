3f20514360a6925b2bc70a0962c829fb29aeea49
Karol Słuszniak <ksluszniak@gmail.com>
2018-05-09 13:35:14 +0200

    Release 0.11.0: fix prefetch count bug

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Protein.Mixfile do
 
   def project do
     [app: :protein,
-     version: "0.10.0",
+     version: "0.11.0",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
